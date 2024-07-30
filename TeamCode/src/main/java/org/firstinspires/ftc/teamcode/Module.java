d74d6766c04592f10a8508def6931832ab0cd020
skandacode <39654761+skandacode@users.noreply.github.com>
2024-07-29 20:07:23 -0500

    added stuff for lampreys

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Module.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Module.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.AnalogInput;
@@ -10,22 +11,34 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Module {
     Motor topMotor, bottomMotor;
     AnalogInput lamprey;
-    double lampreyOffset=0;
-    public Module(HardwareMap hwMap, Telemetry telemetry, String topMotorName, String bottomMotorName, String lampreyName, double lampreyOffset){
+    double lampreyOffset;
+    boolean inverted;
+    public Module(HardwareMap hwMap, Telemetry telemetry, String topMotorName, String bottomMotorName, String lampreyName, double lampreyOffset, boolean inverted){
         topMotor=new MotorEx(hwMap, topMotorName);
         bottomMotor=new MotorEx(hwMap, bottomMotorName);
         lamprey=hwMap.analogInput.get(lampreyName);
         this.lampreyOffset=lampreyOffset;
+        this.inverted=inverted;
     }
     public double getModuleHeading(){
         double before_mod = lamprey.getVoltage()*360/2.189-this.lampreyOffset;
         if (before_mod<0){
             return before_mod+360;
         }
-        return before_mod;
+        return 360-before_mod;
     }
     public void setMotors(double topPower, double bottomPower){
+        topPower = inverted ? -topPower : topPower;
+        bottomPower = inverted ? -bottomPower : bottomPower;
         topMotor.set(topPower);
         bottomMotor.set(bottomPower);
+    }
+    public void setModuleCentricPowers(double forward, double strafe){
+        setMotors(forward+strafe, -forward+strafe);//diffy stuff
+    }
+    public void setRobotCentricPowers(double forward, double strafe){
+        double moduleHeading=getModuleHeading();
+        Vector2d powerVector=new Vector2d(forward, strafe).rotateBy(-moduleHeading);
+        setModuleCentricPowers(powerVector.getX(), powerVector.getY());
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PodTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PodTester.java
@@ -14,8 +14,8 @@ public class PodTester extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
-        left = new Module(hardwareMap, telemetry, "leftTop", "leftBottom", "leftLamprey", 13);
-        right = new Module(hardwareMap, telemetry, "rightTop", "rightBottom", "rightLamprey", 255);
+        left = new Module(hardwareMap, telemetry, "leftTop", "leftBottom", "leftLamprey", 13, false);
+        right = new Module(hardwareMap, telemetry, "rightTop", "rightBottom", "rightLamprey", 255, true);
         waitForStart();
         while (opModeIsActive()){
             left.setMotors(leftTopPower, leftBottomPower);
